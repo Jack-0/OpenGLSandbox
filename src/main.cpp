287ef7493cb2718d279f0679aae9fc7fcb135d5a
jack <jack.jh@protonmail.com>
2020-05-11 19:52:05 +0100

    pressing the 'E' key applies a transformation

--- src/main.cpp
+++ src/main.cpp
@@ -88,12 +88,9 @@ int main()
     // or set it via the texture class
     shader_test.setInt("texture2", 1);
 
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // change poly mode to only show lines
+    // apply matrix transformation
 
-
-
-
-
+    glm::mat4 trans = glm::mat4(1.0f);
 
     // render loop
     while (!glfwWindowShouldClose(window))
@@ -105,11 +102,12 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // apply matrix transformation
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        unsigned int transformLoc = glGetUniformLocation(shader_test.program_id, "transform");
+        unsigned int transformLoc = glGetUniformLocation(shader_test.program_id, "transform");   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // change poly mode to only show lines
+        if(glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        {
+            trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, 0.0f));
+            trans = glm::rotate(trans, 0.1f, glm::vec3(0.0f, 0.0f, 1.0f));
+        }
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         // render the triangle
