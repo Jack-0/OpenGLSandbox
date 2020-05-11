64d7a4ca7fbe2e8b67e6b5f1d4143e4356a117c3
jack <jack.jh@protonmail.com>
2020-05-11 14:49:56 +0100

    added glm into the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
 #include <iostream>
 #include <string>
 #include "Shader.h"
@@ -84,12 +90,20 @@ int main()
 
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // change poly mode to only show lines
 
+    // TODO new
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+
+
+
     // render loop
     while (!glfwWindowShouldClose(window))
     {
         // input
         processInput(window);
-
 
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
