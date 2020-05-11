35196498120957839a611f1d295e35c179aca230
jack <jack.jh@protonmail.com>
2020-05-11 15:56:42 +0100

    implemented matrix transformation

--- src/main.cpp
+++ src/main.cpp
@@ -90,12 +90,8 @@ int main()
 
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // change poly mode to only show lines
 
-    // TODO new
-    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
-    vec = trans * vec;
-    std::cout << vec.x << vec.y << vec.z << std::endl;
+
+
 
 
 
@@ -108,6 +104,13 @@ int main()
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // apply matrix transformation
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        unsigned int transformLoc = glGetUniformLocation(shader_test.program_id, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         // render the triangle
         glActiveTexture(GL_TEXTURE0);
