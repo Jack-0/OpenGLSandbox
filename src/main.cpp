aeb129e85451e1f9d2ae6a494bdfc56a4e9c6070
jack <jack.jh@protonmail.com>
2020-05-23 20:21:38 +0100

    included assimp on a unix machine

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
