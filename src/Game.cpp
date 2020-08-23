090297495f464d76792094284d119e2fb54b6fca
jack-0 <jack.jh@protonmail.com>
2020-08-23 16:00:37 +0100

    refactored main to use Game loop

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,18 +3,135 @@
 //
 
 #include <state/TestState.h>
+#include <std_image.h>
+#include <glad/glad.h>
 #include "Game.h"
+
 
 Game* Game::s_pInstance = 0; // singleton
 
-void Game::init()
+int Game::initGL()
 {
-    m_pGameStateMachine = new GameStateMachine();
-    m_pGameStateMachine->changeState(new TestState());
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    //GLFWwindow* m_window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    m_window = glfwCreateWindow(m_windowWidth, m_windowHeight, "LearnOpenGL", NULL, NULL);
+    if (m_window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(m_window);
+    ///glfwSetFramebufferSizeCallback(m_window, framebuffer_size_callback);
+    ///glfwSetCursorPosCallback(m_window, mouse_callback);
+    ///glfwSetScrollCallback(m_window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    //glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL); // TODO: FOR DEBUG to keep mouse upon error
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    //stbi_set_flip_vertically_on_load(true);
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    return 0;
 }
 
-//void Game::update() {}
+int Game::init(int window_width, int window_height)
+{
+    m_windowWidth = window_width;
+    m_windowHeight = window_height;
 
-//void Game::render() {}
+    if ( initGL() == -1 )
+        return -1;
 
-//void Game::handleEvents() {}
+    // build and compile shaders
+    // -------------------------
+
+    m_pGameStateMachine = new GameStateMachine();
+    m_pGameStateMachine->changeState(new TestState());
+
+    m_running = true;
+
+    //m_cube = new Cube();
+    // TODO
+
+    /*
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+     */
+
+   return 0;
+}
+
+void Game::handleEvents()
+{
+    // if the window is closed end the game loop
+    if( glfwWindowShouldClose(m_window) )
+        m_running = false;
+
+    // if escape is pressed close the window
+    if (glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(m_window, true);
+
+}
+
+void Game::update()
+{
+    // render
+    // ------
+    glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+    // -------------------------------------------------------------------------------
+    glfwSwapBuffers(m_window);
+    glfwPollEvents();
+}
+
+void Game::render()
+{}
+
+void Game::clean()
+{
+    glfwTerminate();
+}
+
+
--- src/Game.h
+++ src/Game.h
@@ -5,7 +5,15 @@
 #ifndef OPENGLSANDBOX_GAME_H
 #define OPENGLSANDBOX_GAME_H
 
+#include <iostream>
 #include <state/GameStateMachine.h>
+
+#include <glad/glad.h> // TODO the order really matters
+#include <GLFW/glfw3.h> // TODO ^
+
+// todo add vbo vao
+// setup to be a opengl wrapper
+#include <graphics/Shader.h>
 
 class Game
 {
@@ -22,25 +30,39 @@ public:
         }
     }
 
-    void init();
-    //void render();
-    //void update();
-    //void handleEvents();
+    int init(int window_width, int window_height);
+    void render();
+    void update();
+    void handleEvents();
+
+    void clean();
+
+    bool running() { return m_running;}
 
     GameStateMachine* getStateMachine() { return m_pGameStateMachine; }
+
+    GLFWwindow* getWindow() { return m_window; }
+
     //int getScreenWidth() { return m_screenWidth; }
     //int getScreenHeight() { return m_screenHeight; }
 
 private:
 
+    int initGL();
+
+    int m_windowWidth;
+    int m_windowHeight;
+
+
     Game() {}
 
-    //int m_screenWidth;
-    //int m_screenHeight;
+    bool m_running = false;
 
     static Game* s_pInstance;
 
     GameStateMachine* m_pGameStateMachine;
+
+    GLFWwindow* m_window;
 };
 
 
