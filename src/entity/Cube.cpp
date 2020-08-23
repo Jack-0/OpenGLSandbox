8e9b2409dc0342e630e519acac723eddf836fd85
jack-0 <jack.jh@protonmail.com>
2020-08-23 15:46:45 +0100

    documented how to build the binary

---
+++ src/entity/Cube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jack on 22/08/2020.
+//
+
+#include "Cube.h"
---
+++ src/entity/Cube.h
@@ -0,0 +1,16 @@
+//
+// Created by jack on 22/08/2020.
+//
+
+#ifndef OPENGLSANDBOX_CUBE_H
+#define OPENGLSANDBOX_CUBE_H
+
+
+
+class Cube {
+
+};
+
+
+
+#endif //OPENGLSANDBOX_CUBE_H
