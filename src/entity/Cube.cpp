f70dd0964851308adf60f9fcf541b9a5fd08d975
jack-0 <jack.jh@protonmail.com>
2020-09-12 13:00:42 +0100

    added a move method

--- src/entity/Cube.cpp
+++ src/entity/Cube.cpp
@@ -25,7 +25,7 @@ void Cube::render()
     m_shader->setMat4("view", view);
     // render the loaded model
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+    model = glm::translate(model, glm::vec3(m_x, m_y, m_z)); // translate it down so it's at the center of the scene
     model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
     m_shader->setMat4("model", model);
     m_model->Draw(*m_shader);
--- src/entity/Cube.h
+++ src/entity/Cube.h
@@ -14,7 +14,17 @@ public:
     void update();
     void render();
     void clean();
-};
 
+    void move(float x, float y, float z)
+    {
+        m_x = x; m_y = y; m_z = z;
+    }
+
+private:
+    float m_x = 0.0f;
+    float m_y = 0.0f;
+    float m_z = 0.0f;
+
+};
 
 #endif //OPENGLSANDBOX_CUBE_H
