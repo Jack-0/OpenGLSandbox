01758a35f7ac682d8fefc4c5fa9223fea6523a56
jack-0 <jack.jh@protonmail.com>
2020-10-12 22:19:32 +0100

    Menu now renders fps text on screen

--- src/state/MenuState.cpp
+++ src/state/MenuState.cpp
@@ -16,19 +16,23 @@ void MenuState::update()
 {
     m_cube->update();
     m_text->update();
+    m_FPStext->setText("FPS: " + to_string(Game::Instance()->getFPS()));
+    m_FPStext->update();
 }
 
 void MenuState::render()
 {
     m_cube->render();
     m_text->render(); // Render text last to ensure it is on the top most layer - i.e not overlapped
+    m_FPStext->render();
 }
 
 bool MenuState::onEnter()
 {
     std::cout  << "State \"" <<s_StateID << "\" loaded!" << std::endl;
     m_cube = new Cube("../res/object/cube/cube.obj","../res/shaders/model_loading.vert","../res/shaders/model_loading.frag");
-    m_text = new Text();
+    m_text = new Text("Hello World", 100, 100, 1.0f, glm::vec3(255,255,255));
+    m_FPStext = new Text("", 0, Game::Instance()->getScreenHeight()- Text::PIXEL_HEIGHT , 1.0f, glm::vec3(255,255,255));
 }
 
 bool MenuState::onExit()
