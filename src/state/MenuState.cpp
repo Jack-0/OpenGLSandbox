6cd1cd931d7b99a63d77ca69595da38cdc5cb2fa
jack-0 <jack.jh@protonmail.com>
2020-10-12 20:21:23 +0100

    rendered "HelloWorld" in the menu state

--- src/state/MenuState.cpp
+++ src/state/MenuState.cpp
@@ -15,17 +15,20 @@ const std::string MenuState::s_StateID = "MENU";
 void MenuState::update()
 {
     m_cube->update();
+    m_text->update();
 }
 
 void MenuState::render()
 {
     m_cube->render();
+    m_text->render(); // Render text last to ensure it is on the top most layer - i.e not overlapped
 }
 
 bool MenuState::onEnter()
 {
     std::cout  << "State \"" <<s_StateID << "\" loaded!" << std::endl;
     m_cube = new Cube("../res/object/cube/cube.obj","../res/shaders/model_loading.vert","../res/shaders/model_loading.frag");
+    m_text = new Text();
 }
 
 bool MenuState::onExit()
