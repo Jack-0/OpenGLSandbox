d570140a615718c480112f1dc41d6905336628a8
jack-0 <jack.jh@protonmail.com>
2020-12-30 22:04:56 +0000

    added component structures

---
+++ src/ecs/components/TransformComponent.h
@@ -0,0 +1,17 @@
+//
+// Created by jack on 30/12/2020.
+//
+
+#ifndef OPENGLSANDBOX_TRANSFORMCOMPONENT_H
+#define OPENGLSANDBOX_TRANSFORMCOMPONENT_H
+
+#include "Math/Vec3.hpp"
+
+struct TransformComponent
+{
+    vec3 pos;
+    vec3 rotation;
+    vec3 scale;
+};
+
+#endif //OPENGLSANDBOX_TRANSFORMCOMPONENT_H
