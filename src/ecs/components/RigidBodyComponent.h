114723e8b61ec4bbd9ee96345efcff5775aaf31e
jack-0 <jack.jh@protonmail.com>
2020-12-30 22:04:56 +0000

    added component structures

---
+++ src/ecs/components/RigidBodyComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by jack on 30/12/2020.
+//
+
+#ifndef OPENGLSANDBOX_RIGIDBODYCOMPONENT_H
+#define OPENGLSANDBOX_RIGIDBODYCOMPONENT_H
+
+#include "Math/Vec3.hpp"
+
+struct RigidBodyComponent
+{
+    vec3 velocity;
+    vec3 acceleration;
+};
+
+#endif //OPENGLSANDBOX_RIGIDBODYCOMPONENT_H
---
+++ src/ecs/components/ShaderComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by jack on 30/12/2020.
+//
+
+#ifndef OPENGLSANDBOX_SHADERCOMPONENT_H
+#define OPENGLSANDBOX_SHADERCOMPONENT_H
+
+
+class ShaderComponent
+{
+    const char *vert_path;
+    const char *frag_path;
+};
+
+
+#endif //OPENGLSANDBOX_SHADERCOMPONENT_H
