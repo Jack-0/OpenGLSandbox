7b4080be807d9d39c1593b2b91d992536729504b
jack-0 <jack.jh@protonmail.com>
2021-01-01 14:38:57 +0000

    changed types to glm::vec3

--- src/ecs/components/TransformComponent.h
+++ src/ecs/components/TransformComponent.h
@@ -5,13 +5,12 @@
 #ifndef OPENGLSANDBOX_TRANSFORMCOMPONENT_H
 #define OPENGLSANDBOX_TRANSFORMCOMPONENT_H
 
-#include "Math/Vec3.hpp"
 
 struct TransformComponent
 {
-    vec3 pos;
-    vec3 rotation;
-    vec3 scale;
+    glm::vec3 pos;
+    glm::vec3 rotation;
+    glm::vec3 scale;
 };
 
 #endif //OPENGLSANDBOX_TRANSFORMCOMPONENT_H
