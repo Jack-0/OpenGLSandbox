9552a2135326be5a0f1294c961b83e64d2982fee
jack-0 <jack.jh@protonmail.com>
2021-01-12 16:56:27 +0000

    removed OOP style entity system references

--- src/state/MenuState.cpp
+++ src/state/MenuState.cpp
@@ -14,27 +14,17 @@ const std::string MenuState::s_StateID = "MENU";
 
 void MenuState::update()
 {
-    m_titleText->update();
-    m_demoText->update();
-    m_exitText->update();
 }
 
 void MenuState::render()
 {
-    m_titleText->render();
-    m_demoText->render();
-    m_exitText->render();
 }
 
 bool MenuState::onEnter()
 {
     std::cout  << "State \"" <<s_StateID << "\" loaded!" << std::endl;
     float scale = 1.0f;
-    m_titleText = new Text("OpenGL Sandbox", Game::Instance()->getScreenWidth() * 0.1, Game::Instance()->getScreenHeight() - (Text::PIXEL_HEIGHT * scale * 2), scale, glm::vec3(255,255,255));
     scale = 0.8f;
-    m_demoText = new Text("> Demo",  Game::Instance()->getScreenWidth() * 0.2, Game::Instance()->getScreenHeight() / 2 + Text::PIXEL_HEIGHT * scale , scale, glm::vec3(255,255,255));
-    m_exitText = new Text("> Exit", Game::Instance()->getScreenWidth() * 0.2, Game::Instance()->getScreenHeight() / 2 - Text::PIXEL_HEIGHT * scale , scale, glm::vec3(255,255,255));
-
 }
 
 bool MenuState::onExit()
