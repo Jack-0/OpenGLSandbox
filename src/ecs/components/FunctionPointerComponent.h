a3384f0a53eaf8539ce72f730760502af16586bf
jack-0 <jack.jh@protonmail.com>
2021-01-13 20:43:23 +0000

    added FunctionPointerComponent to handle function callbacks

---
+++ src/ecs/components/FunctionPointerComponent.h
@@ -0,0 +1,14 @@
+//
+// Created by jack on 13/01/2021.
+//
+
+#ifndef OPENGLSANDBOX_FUNCTIONPOINTERCOMPONENT_H
+#define OPENGLSANDBOX_FUNCTIONPOINTERCOMPONENT_H
+
+struct FunctionPointerComponent
+{
+    //int id;
+    void (*fp)(); // function pointer
+};
+
+#endif //OPENGLSANDBOX_FUNCTIONPOINTERCOMPONENT_H
