72b0dd827564495043aab6f6be403c4f12015e22
jack-0 <jack.jh@protonmail.com>
2021-03-14 19:25:46 +0000

    added debug code to monitor performance

--- src/state/TestState.cpp
+++ src/state/TestState.cpp
@@ -15,6 +15,8 @@
 #include <ecs/components/TextComponent.h>
 #include <ecs/components/Dimensions2DComponent.h>
 
+#include <chrono>
+
 const std::string TestState::s_StateID = "TEST";
 
 void TestState::update()
@@ -62,13 +64,40 @@ bool TestState::onEnter(){
     std::default_random_engine rand;
     std::uniform_real_distribution<float> rand_rot(0.0f, 360.0f);
     
-    float x = -4.5;
-    for(int i = 0; i < 4; i++)
-    {
-        
-        glm::vec3 pos = {x, 0, -10};
+    // TODO debug time
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
+    auto t1 = high_resolution_clock::now(); // start time = t1
+
+    float x_reset = -9;
+    float x = x_reset;
+    float y = 0;
+    
+    
+    for(int i = 0; i < 500; i++)
+    {    
+        // skip the first cube to create a cube grid
+        if( i != 0)
+        {
+            // alter position vector values for each cube
+            if (i % 8 == 0)
+            {
+                x = x_reset;
+                y -= 4.5;
+            }
+            else
+            {
+                x += 3;
+            }
+        }
+
+        glm::vec3 pos = {x, y, -10};
         glm::vec3 rotation = {rand_rot(rand),rand_rot(rand),rand_rot(rand)};
         glm::vec3 scale = {1.0f, 1.0f, 1.0f};
+
         
         Entity cube = Game::Instance()->get_ecs()->create_entity();
         Game::Instance()->get_ecs()->add_component_to_entity(cube, MeshComponent{"../res/object/cube/cube.obj", NULL});
@@ -76,9 +105,18 @@ bool TestState::onEnter(){
                                                                                    "../res/shaders/model_loading.frag",
                                                                                    NULL});
         Game::Instance()->get_ecs()->add_component_to_entity(cube, TransformComponent{pos,rotation,scale});
-        x += 3;
     }
+    auto t2 = high_resolution_clock::now();
     mesh_renderer->init();
+    auto t3 = high_resolution_clock::now();
+
+    duration<double, std::milli> ms_loop = t2 - t1;
+    duration<double, std::milli> ms_mesh = t3 - t2;
+    duration<double, std::milli> ms_total = t3 - t1;
+
+    std::cout << "Init time results:\n" << "Time to finish loop = " << ms_loop.count() << "ms\n"
+    << "Time to finish mesh init() = " << ms_mesh.count() << "ms\n" 
+    << "Total time = " << ms_total.count() << "ms\n";
 }
 
 bool TestState::onExit()
