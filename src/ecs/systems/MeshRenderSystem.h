33ee202aedc97a66e1dcbca0373359bd1b2da702
jack-0 <jack.jh@protonmail.com>
2021-03-14 19:24:37 +0000

    added a map to reduce mesh init time

--- src/ecs/systems/MeshRenderSystem.h
+++ src/ecs/systems/MeshRenderSystem.h
@@ -7,12 +7,20 @@
 
 
 #include <ecs/core/System.h>
+#include <map>
+#include <string>
+#include <util/Model.h>
+#include <memory>
 
 class MeshRenderSystem : public System
 {
 public:
     void init();
     void render();
+
+private:
+    // map string names to models to avoid long init times
+    std::map<std::string, Model*> models;
 };
 
 #endif //OPENGLSANDBOX_RENDERSYSTEM_H
